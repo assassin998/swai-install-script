1328c1de0ce77433f713fbf2a25ff898c5a83c4f
assassin <82395213+assassin998@users.noreply.github.com>
2022-10-05 16:50:38 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+clear
+echo "starting"
+sudo pacman -Syuyu
+sudo pacman -S sway wayland xorg-wayland wofi pipewire 
+sudo pacman -S brightnessctl grim thunar feh slurp kitty firefox swaync pipewire-pulse zsh
+echo "done"
+
+sleep 2
+clear
+
+read -p "enter user: " user
+command -v zsh | sudo tee -a /etc/shells
+sudo chsh -s "$(command -v zsh)" "$user"
+echo "done, your deafult shell is zsh"
+
+read -p "enter"
+sleep 2
+clear
+
+sudo cp -r  ~/Downloads/config/* ~/.config/
+cp -r  ~/Downloads/Pictures/* ~/Pictures/
+
+echo "done"
+read -p "press to end"
+clear
+sleep 2
